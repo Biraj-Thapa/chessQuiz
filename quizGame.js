de31650b7c9079e34ab13b06505bcb1fc32e016b
Biraj Thapa <birajthapa983@gmail.com>
2024-01-19 13:21:20 +0545

    added your correct numers

--- quizGame.js
+++ quizGame.js
@@ -1,9 +1,13 @@
 const prompt=require("prompt-sync")();
 console.log("welcome to the Quiz about Chess");
+let correctAnswers=0;
+
 const question1=prompt("How many players are there in chess ");
 const  answer1 = "2";
 if(question1===answer1){
-    console.log("The answer is correct")
+    console.log("The answer is correct");
+    correctAnswers=correctAnswers+1;
+
 }
 else{
     console.log("wrong Answer")
@@ -12,6 +16,7 @@ const question2=prompt("How many pieces does each black and white have in chess 
 const  answer2 = "16";
 if(question2===answer2){
     console.log("The answer is correct")
+    correctAnswers=correctAnswers+1;
 }
 else{
     console.log("wrong Answer")
@@ -20,6 +25,7 @@ const question3=prompt("How many total pieces are there in chess ");
 const  answer3 = "32";
 if(question3===answer3){
     console.log("The answer is correct")
+    correctAnswers=correctAnswers+1;
 }
 else{
     console.log("wrong Answer")
@@ -28,6 +34,7 @@ const question4=prompt("How many total pawns are there in chess ");
 const  answer4 = "16";
 if(question4===answer4){
     console.log("The answer is correct")
+    correctAnswers=correctAnswers+1;
 }
 else{
     console.log("wrong Answer")
@@ -36,6 +43,7 @@ const question5=prompt("How many rooks does white have in chess ");
 const  answer5 = "2";
 if(question5===answer5){
     console.log("The answer is correct")
+    correctAnswers=correctAnswers+1;
 }
 else{
     console.log("wrong Answer")
@@ -44,7 +52,9 @@ const question6=prompt("How many bishop does black have in chess ");
 const  answer6 = "2";
 if(question6===answer6){
     console.log("The answer is correct")
+    correctAnswers=correctAnswers+1;
 }
 else{
     console.log("wrong Answer")
 }
+console.log(`You have answered ${correctAnswers}  questions correctly`)
