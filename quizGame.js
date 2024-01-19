e246d9e3a3ea3212e91e62425452492708a9d8a0
Biraj Thapa <birajthapa983@gmail.com>
2024-01-19 13:05:13 +0545

    6 questions added

---
+++ quizGame.js
@@ -0,0 +1,50 @@
+const prompt=require("prompt-sync")();
+console.log("welcome to the Quiz about Chess");
+const question1=prompt("How many players are there in chess ");
+const  answer1 = "2";
+if(question1===answer1){
+    console.log("The answer is correct")
+}
+else{
+    console.log("wrong Answer")
+}
+const question2=prompt("How many pieces does each black and white have in chess ");
+const  answer2 = "16";
+if(question2===answer2){
+    console.log("The answer is correct")
+}
+else{
+    console.log("wrong Answer")
+}
+const question3=prompt("How many total pieces are there in chess ");
+const  answer3 = "32";
+if(question3===answer3){
+    console.log("The answer is correct")
+}
+else{
+    console.log("wrong Answer")
+}
+const question4=prompt("How many total pawns are there in chess ");
+const  answer4 = "16";
+if(question4===answer4){
+    console.log("The answer is correct")
+}
+else{
+    console.log("wrong Answer")
+}
+const question5=prompt("How many rooks does white have in chess ");
+const  answer5 = "2";
+if(question5===answer5){
+    console.log("The answer is correct")
+}
+else{
+    console.log("wrong Answer")
+}
+const question6=prompt("How many bishop does black have in chess ");
+const  answer6 = "2";
+if(question6===answer6){
+    console.log("The answer is correct")
+}
+else{
+    console.log("wrong Answer")
+}
