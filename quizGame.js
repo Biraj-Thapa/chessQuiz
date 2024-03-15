4cded5d85ac2506517c03b7807c7bec1b93db94e
Biraj Thapa <birajthapa983@gmail.com>
2024-03-15 11:30:32 +0545

    added new questions

--- quizGame.js
+++ quizGame.js
@@ -57,4 +57,22 @@ if(question6===answer6){
 else{
     console.log("wrong Answer")
 }
+const question7=prompt("How many knight does black have in chess ");
+const  answer7 = "2";
+if(question7===answer7){
+    console.log("The answer is correct")
+    correctAnswers=correctAnswers+1;
+}
+else{
+    console.log("wrong Answer")
+}
+const question8=prompt("How many queen does white have in chess ");
+const  answer8 = "1";
+if(question8===answer8){
+    console.log("The answer is correct")
+    correctAnswers=correctAnswers+1;
+}
+else{
+    console.log("wrong Answer")
+}
 console.log(`You have answered ${correctAnswers}  questions correctly`)
